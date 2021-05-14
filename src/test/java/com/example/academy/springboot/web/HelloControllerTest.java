e6cfdef17608b3785e1d5238e1e6f810dcdda659
ltk <xorud2592@gmail.com>
2021-05-14 16:38:29 +0900

    Initial commit

---
+++ src/test/java/com/example/academy/springboot/web/HelloControllerTest.java
@@ -0,0 +1,32 @@
+package com.example.academy.springboot.web;
+
+//import com.example.web.springboot.config.auth.SecurityConfig;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class, secure = false)
+//@ContextConfiguration
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void  returnHello() throws  Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+}
